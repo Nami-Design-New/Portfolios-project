ba15ebde8d5e45eb417521ab4c39a5201c4f1724
mahmoudelkomy996 <mahmoudelkomy996@gmail.com>
2023-02-04 17:13:14 +0200

    3

---
+++ js/custom.js
@@ -0,0 +1,61 @@
+$(document).ready(function () {
+  // nav menu
+  $(".navBtn").click(function () {
+    $(this).toggleClass("active");
+    $(".navMenu").toggleClass("active");
+  });
+  //navbar ainmation
+  $(window).scroll(function () {
+    var appScroll = $(document).scrollTop();
+    if (appScroll >= 1) {
+      $("header").addClass("headerAnimate");
+    } else {
+      $("header").removeClass("headerAnimate");
+    }
+  });
+
+});
+// ////////////////////////////////////////
+// ////////////////////////////////////////
+// ////////////////////////////////////////
+// ////////////////////////////////////////
+// ////////////////////////////////////////
+// ////////////////////////////////////////
+// ////////////////////////////////////////
+// ////////////////////////////////////////
+// ////////////////////////////////////////
+// ////////////////////////////////////////
+// ////////////////////////////////////////
+// ////////////////////////////////////////
+$(document).ready(function () {
+  //spinner
+  $(".spinner").fadeOut("slow");
+  // odometer
+  $(".odometer").appear(function (e) {
+    var odo = $(".odometer");
+    odo.each(function () {
+      var countNumber = $(this).attr("data-count");
+      $(this).html(countNumber);
+    });
+  });
+  // select2
+  $(".select2search").select2();
+  $(".select2").select2({
+    minimumResultsForSearch: -1,
+  });
+  //aos Delay
+  $("section").each(function () {
+    const sectionDivs = $(this).find("[data-aos]");
+    sectionDivs.each(function (index) {
+      $(this).attr("data-aos-delay", (index + 1) * 150);
+    });
+  });
+  // aos
+  AOS.init({
+    offset: 20,
+    delay: 50,
+    duration: 1000,
+    // easing: "linear",
+    once: false,
+  });
+});
